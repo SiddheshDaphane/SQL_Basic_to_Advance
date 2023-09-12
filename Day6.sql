c14e655deb8dacd8c9f1a4ba545dfd28d2dc2932
SiddheshDaphane <siddhesh.daphane@gmail.com>
2023-09-12 07:57:35 -0500

    Self Join, CASE WHEN and STRING functions

---
+++ Day6.sql
@@ -0,0 +1,11 @@
+
+SELECT * FROM employee
+-- In this employee table, manager_id is referring to employee-id meaning table is referring to itself
+
+SELECT * FROM dept
+
+-- I want to know the manager name of each employee
+
+SELECT emp_name, 
+
+
