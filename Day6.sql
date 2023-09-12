48a4aa6b247854a7a5956558597f5168b5b84c91
SiddheshDaphane <siddhesh.daphane@gmail.com>
2023-09-12 07:58:30 -0500

    Self Join, CASE WHEN and STRING functions

--- Day6.sql
+++ Day6.sql
@@ -6,6 +6,16 @@ SELECT * FROM dept
 
 -- I want to know the manager name of each employee
 
-SELECT emp_name, 
+SELECT e1.emp_id, e1.emp_name, e2.emp_id as manager_id, e2.emp_name as manager_name 
+FROM employee e1
+INNER JOIN employee e2 
+ON e1.manager_id = e2.emp_id
 
+
+-- Find the employee whose salary is greater than his/her manager
+SELECT e1.emp_id, e1.emp_name, e2.emp_id as manager_id, e2.emp_name as manager_name, e1.salary
+FROM employee e1
+INNER JOIN employee e2 
+ON e1.manager_id = e2.emp_id
+WHERE e1.salary > e2.salary 
 
