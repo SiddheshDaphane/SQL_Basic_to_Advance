3a2888fe999285251a93c2b128d7dfe02ff8f957
SiddheshDaphane <siddhesh.daphane@gmail.com>
2023-09-17 20:08:43 -0500

    Self-join explanation

--- Day6.sql
+++ Day6.sql
@@ -6,6 +6,13 @@ SELECT * FROM dept
 
 -- I want to know the manager name of each employee
 
+/* When you do self-join, always imagine that there are two tables. Now you need to be extremely careful when joining the tables. Because that will change the output.
+In this case, we are joining e1.manager_id to e2.emp_id because we want the manager of the employee, and hence we must join the manager_id of the 1st table to the employee_id of the 2nd table.
+Now it is very important to know from which table you are taking employee_name and manager_name. We are joining manager_id of the first table to employee_id of a second table, which means
+We should take employee_name from the first table and manager_name from the second table.
+   */
+
+
 SELECT e1.emp_id, e1.emp_name, e2.emp_id as manager_id, e2.emp_name as manager_name 
 FROM employee e1
 INNER JOIN employee e2 
@@ -18,6 +25,9 @@ FROM employee e1
 INNER JOIN employee e2 
 ON e1.manager_id = e2.emp_id
 WHERE e1.salary > e2.salary 
+
+
+
 
 
 
