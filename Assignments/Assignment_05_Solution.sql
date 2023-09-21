e073b10c8e43b7b71b05272081a4a7407e11bab4
SiddheshDaphane <siddhesh.daphane@gmail.com>
2023-09-21 15:33:11 -0500

    Assignment 05 Solution
    Tried 3rd question without Window function but I think it is not possible

--- Assignments/Assignment_05_Solution.sql
+++ Assignments/Assignment_05_Solution.sql
@@ -59,3 +59,47 @@ select customer_name , trim(SUBSTRING(customer_name,1,CHARINDEX(' ',customer_nam
 , SUBSTRING(customer_name,CHARINDEX(' ',customer_name)+1,len(customer_name)-CHARINDEX(' ',customer_name)+1) as second_name
 from orders
 
+
+
+create table drivers(id varchar(10), start_time time, end_time time, start_loc varchar(10), end_loc varchar(10));
+insert into drivers values('dri_1', '09:00', '09:30', 'a','b'),('dri_1', '09:30', '10:30', 'b','c'),('dri_1','11:00','11:30', 'd','e');
+insert into drivers values('dri_1', '12:00', '12:30', 'f','g'),('dri_1', '13:30', '14:30', 'c','h');
+insert into drivers values('dri_2', '12:15', '12:30', 'f','g'),('dri_2', '13:30', '14:30', 'c', 'h');
+
+SELECT * FROM drivers
+
+
+-- Q3) write a query to print below output using drivers table. Profit rides are the no of rides where end location of a ride is same as start location of immediate next ride for a driver
+-- id, total_rides , profit_rides
+-- dri_1,5,1
+-- dri_2,2,0
+
+-- My Solution
+SELECT d1.id, COUNT(d1.id), COUNT(d2.id)
+FROM drivers d1 
+LEFT JOIN drivers d2 
+ON d1.id = d2.id AND d1.end_loc = d2.start_loc
+GROUP BY d1.id
+
+/* I want to try to solve this question without using window fucntion because I have not learned any window functions till now
+but I think it is not possible to solve this question. Regardless, I have given a solution which uses window fucntion. */
+
+-- Solution
+--lead function window
+select id, count(1) as total_rides
+,sum(case when end_loc=next_start_location then 1 else 0 end ) as profit_rides
+from (
+select *
+, lead(start_loc,1) over(partition by id order by start_time asc) as next_start_location
+from drivers) A
+group by id;
+
+--self join
+with rides as (
+select *,row_number() over(partition by id order by start_time asc) as rn
+from drivers)
+select r1.id , count(1) total_rides, count(r2.id) as profit_rides
+from rides r1
+left join rides r2
+on r1.id=r2.id and r1.end_loc=r2.start_loc and r1.rn+1=r2.rn
+group by r1.id
