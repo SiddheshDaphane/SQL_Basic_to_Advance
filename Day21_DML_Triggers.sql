0a5b735f51d8511f88c1779e6cbe9a0af15a9f30
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-11-06 08:15:44 -0600

    DML Triggers

--- Day21_DML_Triggers.sql
+++ Day21_DML_Triggers.sql
@@ -34,6 +34,39 @@ DELETE FROM colors
 WHERE id = 4
 
 
+--------------------------------------- Modifying Triggers ---------------------------------------
 
 
+USE namasteSQL
+GO
 
+ALTER TRIGGER trgColor
+ON colors
+AFTER INSERT, UPDATE, DELETE
+AS 
+BEGIN
+    PRINT 'Data was changed in the color table'
+END
+
+
+GO
+
+-- Inserting into a table
+INSERT INTO colors VALUES(4, 'Black')
+
+-- Updating a value of a table
+UPDATE colors 
+SET color = 'Black2'
+WHERE id = 4
+
+select * from colors
+
+-- Deleting a record from a table 
+
+DELETE FROM colors
+WHERE id = 4
+
+
+----------------------------------- Deleting triggers ----------------------------------------
+
+DROP TRIGGER trgColor
