9831873f259edf7b3839b132d80b72945d435be6
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-11-06 10:01:03 -0600

    DDL Triggers

--- Day22_DDL_Triggers.sql
+++ Day22_DDL_Triggers.sql
@@ -19,3 +19,24 @@ END
 CREATE TABLE test(ID int)
 
 
+------------------------------- Modifying a Trigger -----------------------------
+GO
+
+ALTER TRIGGER trgNoNewTables
+ON DATABASE
+FOR CREATE_TABLE, DROP_TABLE, ALTER_TABLE
+AS
+BEGIN
+    PRINT 'No changes to tables'
+    ROLLBACK
+END
+
+
+----- Testing a trigger 
+
+CREATE TABLE test(ID int)
+
+DROP TABLE t1
+
+
+-------------------------------------- Deleting a trigger ---------------------------------------------
