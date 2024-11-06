46ca878036c4a75614062189328337ece24ade93
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-11-06 08:11:28 -0600

    DML Triggers

---
+++ Day21_DML_Triggers.sql
@@ -0,0 +1,39 @@
+select * from colors
+
+
+
+---------------------------------------------- Creating Triggers ------------------------------------------
+
+USE namasteSQL
+GO
+
+CREATE TRIGGER trgColor
+ON colors
+AFTER INSERT, UPDATE, DELETE
+AS 
+BEGIN
+    PRINT 'Something happened to color table'
+END
+
+
+GO
+
+-- Inserting into a table
+INSERT INTO colors VALUES(4, 'Black')
+
+-- Updating a value of a table
+UPDATE colors 
+SET color = 'Black2'
+WHERE id = 4
+
+select * from colors
+
+-- Deleting a record from a table 
+
+DELETE FROM colors
+WHERE id = 4
+
+
+
+
+
